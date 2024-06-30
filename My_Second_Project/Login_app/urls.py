8fcd2655bd2079edabe5d3016f6f922a19ae3c9f
Abir <mdtanimurabir0@gmail.com>
2024-06-30 13:18:31 +0600

    User Authentication

---
+++ My_Second_Project/Login_app/urls.py
@@ -0,0 +1,12 @@
+# from django.conf.urls import url
+from django.urls import path
+from Login_app import views
+
+# from django.urls import path
+# from . import views
+
+app_name = 'Login_app'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
