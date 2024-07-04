18cad9b1e26b9c9342882efb87f77572773e172d
Abir <mdtanimurabir0@gmail.com>
2024-07-04 14:41:25 +0600

    User Authentication

--- My_Second_Project/Login_app/views.py
+++ My_Second_Project/Login_app/views.py
@@ -10,7 +10,32 @@ def index(request):
 
 
 def register(request):
+
+    registerd = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        user_info_form = UserInfoForm(data=request.POST)
+
+        if user_form.is_valid() and user_info_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            user_info = user_info_form.save(commit=False)
+            user_info.user = user
+
+            if 'profile_pics' in request.FILES:
+                user_info.profile_pics = request.FILES['profile_pics']
+                
+            user_info.save()
+
+            registerd = True
+
+    else:
+        user_form = UserForm()
+        user_info_form = UserInfoForm()
     user_form = UserForm()
     user_info_form = UserInfoForm()
-    dict = {'user_form': user_form, 'user_info_form': user_info_form}
+    dict = {'user_form': user_form, 'user_info_form': user_info_form, 'registerd':registerd,}
     return render(request, 'Login_app/register.html', context=dict)
